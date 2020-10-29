10edd1b9f76763a3e91cacf4aef61ae3415da04b
nino2806 <oo.nino193@gmail.com>
2020-10-29 14:30:55 +0700

    update DNS

--- script_web.sh
+++ script_web.sh
@@ -1,1 +1,2 @@
-gunicorn --bind 127.0.0.1:8001 myproject.wsgi:application --access-logfile='-' --error-logfile='-' --access-logformat '%({X-Forwarded-For}i)s %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s"'
+gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application --access-logfile='-' --error-logfile='-' --access-logformat '%({X-Forwarded-For}i)s %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s"'
+
